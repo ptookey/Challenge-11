0e33cf63905cb78024d6d839ec137de5bfe33e77
tookey95 <petetookey@gmail.com>
2023-07-08 12:25:36 -0400

    added server.js and basic functionality

---
+++ Develop/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static('public'));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
