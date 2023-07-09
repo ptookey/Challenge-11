f92abaa8752472253ab5d444510e942b6909e9d1
tookey95 <petetookey@gmail.com>
2023-07-09 12:21:35 -0400

    added * route

--- Develop/server.js
+++ Develop/server.js
@@ -82,6 +82,10 @@ app.delete('/api/notes/:id', (req, res) => {
   })
 });
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
